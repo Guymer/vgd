8ce0d7ed93ff01ea9317248e1f484735ec2eda1a
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-01-30 15:54:05 +0000

    add comment

--- src/mod_funcs.F90
+++ src/mod_funcs.F90
@@ -1,11 +1,11 @@
 MODULE mod_funcs
     CONTAINS
 
+    ! Include subroutines ...
     INCLUDE "mod_funcs/sub_go_east.f90"
     INCLUDE "mod_funcs/sub_go_north.f90"
     INCLUDE "mod_funcs/sub_go_south.f90"
     INCLUDE "mod_funcs/sub_go_west.f90"
-
     INCLUDE "mod_funcs/sub_going_east.f90"
     INCLUDE "mod_funcs/sub_going_north.f90"
     INCLUDE "mod_funcs/sub_going_south.f90"
