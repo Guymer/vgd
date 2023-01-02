945bcdf3426d32b84b9801368b5fceebc7678713
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2023-01-02 10:44:09 +0000

    use standard import block

--- convertCSVtoGeoJSON.py
+++ convertCSVtoGeoJSON.py
@@ -7,6 +7,7 @@ import os
 # Import special modules ...
 try:
     import geojson
+    geojson.geometry.Geometry.__init__.__defaults__ = (None, False, 12)     # NOTE: See https://github.com/jazzband/geojson/issues/135#issuecomment-596509669
 except:
     raise Exception("\"geojson\" is not installed; run \"pip install --user geojson\"") from None
 try:
