2aee4d80f91db8a9f0dd4cee3381e308c6dfb73f
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2024-11-23 16:48:01 +0000

    allow one more scale

--- convertBINtoPNG.py
+++ convertBINtoPNG.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
         mega = float((nx // scale) * (ny // scale)) / 1.0e6                     # [Mpx]
 
         # Skip this scale if the PNG would be too big ...
-        if mega > 64.0:
+        if mega > 250.0:
             print(f"Skipping \"{iname}\" (the PNG would be {mega:.1f} Mpx).")
             continue
 
